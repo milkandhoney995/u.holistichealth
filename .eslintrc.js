fed399dbc3970304e16dc355be1c0f19708459d1
harada.utano <harada.utano@cri-mw.co.jp>
2022-02-14 11:46:43 +0900

    eslintとprettier, modern-css-resetの設定

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true, // プロジェクトのルートに配置していると教えている
+    env: {
+        es2020: true,
+        node: true,
+    },
+    parser: "@typescript-eslint/parser", // ESLintにTypeScriptを適応
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2020,
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.eslint.json"],
+    },
+    plugins: ["@typescript-eslint"], // TypeScriptプラグインのルールを適用
+    extends: [
+        "eslint:recommended", //ESLintのJavaScriptルールを適用
+        "plugin:@typescript-eslint/recommended", // 型チェックが不要なルールを適用
+        "prettier",
+        "prettier/@typescript-eslint", // Prettierでカバーできるルールを無効化
+    ],
+    rules: {},
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,38 @@
 import React from "react";
+import { AppProps } from "next/app";
 import "../styles/globals.scss";
+import "modern-css-reset/dist/reset.min.css";
 import Layout from "../components/layout/layout";
 import LayoutHealth from "../components/layout/layout_health";
 import LayoutWeb from "../components/layout/layout_web";
 
-function App({ Component, pageProps }) {
-  switch (pageProps.layout) {
-    case "main": {
-      return (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      );
+function App({ Component, pageProps }: AppProps) {
+    switch (pageProps.layout) {
+        case "main": {
+            return (
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            );
+        }
+        case "health": {
+            return (
+                <LayoutHealth>
+                    <Component {...pageProps} />
+                </LayoutHealth>
+            );
+        }
+        case "web": {
+            return (
+                <LayoutWeb>
+                    <Component {...pageProps} />
+                </LayoutWeb>
+            );
+        }
+        default: {
+            return <Component {...pageProps} />;
+        }
     }
-    case "health": {
-      return (
-        <LayoutHealth>
-          <Component {...pageProps} />
-        </LayoutHealth>
-      );
-    }
-    case "web": {
-      return (
-        <LayoutWeb>
-          <Component {...pageProps} />
-        </LayoutWeb>
-      );
-    }
-    default: {
-      return <Component {...pageProps} />;
-    }
-  }
 }
 
 export default App;
