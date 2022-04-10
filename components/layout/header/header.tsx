518ed426b975745abdfc36f876375bd96eeb31e5
harada.utano <harada.utano@cri-mw.co.jp>
2022-04-10 21:44:27 +0900

    不要なコメントアウト・クラスを削除

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import classes from "./header.module.scss";
-import Logo from "../logo/logo";
 import Navigation from "../navigation/navigation";
 
 function Header() {
