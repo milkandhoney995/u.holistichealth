046a20e3f31d08ea229bc937fb6cac95cff24448
milkandhoney995 <harada.g17@gmail.com>
2023-04-28 20:46:10 +0900

    indentation, mixin

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -2,11 +2,11 @@ import classes from "./header.module.scss";
 import Navigation from "../navigation/navigation";
 
 function Header() {
-    return (
-        <header id="header" className={classes.header}>
-            <Navigation />
-        </header>
-    );
+  return (
+    <header id="header" className={classes.header}>
+      <Navigation />
+    </header>
+  );
 }
 
 export default Header;
--- pages/webdeveloper/portfolio/index.tsx
+++ pages/webdeveloper/portfolio/index.tsx
@@ -4,51 +4,51 @@ import Link from "next/link";
 import { Fragment } from "react";
 
 export default function Portofolio() {
-    return (
-        <Fragment>
-            <Head>
-                <title>u.holistichealth</title>
-                <meta name="description" content="this is u.holistichealth's website." />
-            </Head>
-            <div className="section">
-                <Image
-                    objectFit="cover"
-                    src="/images/life/cafe.jpg"
-                    width={500}
-                    height={500}
-                    alt="debug"
-                />
-                <div>
-                    <h2>Utano Harada</h2>
-                    <h3 className="title--small">Holistic Health Coach & Web Frontend Engineer</h3>
-                    <p className="container__text">
-                        1997年千葉出身。<br></br>
-                        国際基督教大学教養学部化学メジャー卒。
-                        <br></br>
-                        <br></br>
-                        某IT企業でWeb フロントエンドエンジニアとしてアプリケーション開発に携わる。
-                        <br></br>
-                        現在、フリーランスのWeb
-                        フロントエンドエンジニア兼米国認定ホリスティックヘルスコーチとして活動中。
-                        <br></br>
-                        <br></br>
-                        ヘルスコーチとしての活動は、
-                        <Link href="/holistichealthcoach">
-                            <a>こちら</a>
-                        </Link>
-                        をご参照ください。
-                        <br></br>
-                    </p>
-                </div>
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Head>
+        <title>u.holistichealth</title>
+        <meta name="description" content="this is u.holistichealth's website." />
+      </Head>
+      <div className="section">
+        <Image
+          objectFit="cover"
+          src="/images/life/cafe.jpg"
+          width={500}
+          height={500}
+          alt="debug"
+        />
+        <div>
+          <h2>Utano Harada</h2>
+          <h3 className="title--small">Holistic Health Coach & Web Frontend Engineer</h3>
+          <p className="container__text">
+            1997年千葉出身。<br></br>
+            国際基督教大学教養学部化学メジャー卒。
+            <br></br>
+            <br></br>
+            某IT企業でWeb フロントエンドエンジニアとしてアプリケーション開発に携わる。
+            <br></br>
+            現在、フリーランスのWeb
+            フロントエンドエンジニア兼米国認定ホリスティックヘルスコーチとして活動中。
+            <br></br>
+            <br></br>
+            ヘルスコーチとしての活動は、
+            <Link href="/holistichealthcoach">
+                <a>こちら</a>
+            </Link>
+            をご参照ください。
+            <br></br>
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            layout: "web",
-        },
-    };
+  return {
+    props: {
+      layout: "web",
+    },
+  };
 }
