626a18b3e44b0f2efb2078ae7cd9284833e9d370
milkandhoney995 <harada.g17@gmail.com>
2023-04-30 19:43:02 +0900

    Sectionコンポーネントに置き換え

--- pages/holistichealthcoach/service/index.tsx
+++ pages/holistichealthcoach/service/index.tsx
@@ -1,118 +1,77 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Fragment } from "react";
+import { Section } from "../../../components/section/section";
 
 export default function Service() {
-    return (
-        <Fragment>
-            <Head>
-                <title>u.holistichealth</title>
-                <meta name="description" content="this is u.holistichealth's website." />
-            </Head>
-            <div className="main">
-                <div className="sectionWrap">
-                    <h2 className="text--center">変化を習慣化する</h2>
-                    <div className="section section--reverse">
-                        <Image
-                            style={{ objectFit: 'contain' }}
-                            src="/images/life/meal.jpg"
-                            width={300}
-                            height={300}
-                            alt="debug"
-                        />
-                        <p className="section__body">
-                            食事、マインドセット、人間関係など、<br></br>
-                            何事も新しいことを定着させるには、時間がかかります。<br></br>
-                            <br></br>
-                            自分に厳しくしすぎて、うまくいかなかった経験がある方もいらっしゃるのではないでしょうか。
-                            <br></br>
-                            <br></br>
-                            私のコーチングでは、ヒアリングをもとに、<br></br>
-                            できることから少しずつ習慣化できるようサポートします。
-                        </p>
-                    </div>
-                </div>
-                <div className="sectionWrap">
-                    <h2 className="text--center">詳細</h2>
-                    <div className="section">
-                        <Image
-                            style={{ objectFit: 'contain' }}
-                            src="/images/life/meal.jpg"
-                            width={300}
-                            height={300}
-                            alt="debug"
-                        />
-
-                        <p className="section__body">
-                            セッションは対面形式です。<br></br>
-                            ＊オンライン希望の場合、ご相談ください。<br></br>
-                            <br></br>
-                            カフェなどお好きな場所をご指定ください。<br></br>
-                            私はカフェ巡りが大好きなので、素敵なところで一緒にお話しできたらうれしいです。
-                            <br></br>
-                            ＊交通費や飲食代は料金に含まれませんので、ご了承ください。
-                        </p>
-                    </div>
-                </div>
-                <div className="sectionWrap">
-                    <h2 className="text--center">1:1 90 Minute Session</h2>
-                    <div className="twoColumns_container1">
-                        <Image
-                            style={{ objectFit: 'contain' }}
-                            src="/images/life/veg1.jpg"
-                            width={600}
-                            height={600}
-                            alt="debug"
-                        />
-
-                        <div>
-                            <p className="section__body">
-                                セッションの前日までにワークシートを記入していただきます。
-                                <br></br>
-                                <br></br>
-                                記入していただいた内容をベースに、過去と現在の食生活、人間関係、環境の変化など多方面から今の自分を深掘りしていきます。{" "}
-                                <br></br>
-                                <br></br>
-                                話を聞いてほしい、ちょっと試してみたいなど理由はなんでも構いません。
-                                <br></br>
-                                <br></br>1セッション50分<br></br>
-                                初回無料、2回目以降3,000円。
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* <h2 className="text--center">6 month program</h2> */}
-            {/* <div className="twoColumns_container1">
-                <Image
-                    style={{ objectFit: 'contain' }}
-                    src="/images/life/veg1.jpg"
-                    width={600}
-                    height={600}
-                    alt="debug"
-                />
-                <div>
-                    <p className="section__body">
-                        もっと健康になりたい、もっと幸せになりたい人のためのプログラムです。
-                        <br></br>
-                        <br></br>
-                        最初のセッションで、今のあなたを知るワークを行い、<br></br>
-                        ひとりひとりの目的に合わせて月2回、6ヶ月のプログラムを考えます。
-                        <br></br>
-                        定期的にセッションを受けることで、知識が習慣化しやすくなるだけでなく、自分自身の変化をより実感することができます。
-                    </p>
-                    <Button href='"/holistichealthcoach/service"' label="Detail" />
-                </div>
-            </div> */}
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Head>
+        <title>u.holistichealth</title>
+        <meta name="description" content="this is u.holistichealth's website." />
+      </Head>
+      <div className="main">
+        <Section
+          img={"/images/life/meal.jpg"}
+          width={300}
+          height={300}
+          reverse={true}
+        >
+          <Section.Title>変化を習慣化する</Section.Title>
+          <Section.Body>
+            食事、マインドセット、人間関係など、<br></br>
+            何事も新しいことを定着させるには、時間がかかります。<br></br>
+            <br></br>
+            自分に厳しくしすぎて、うまくいかなかった経験がある方もいらっしゃるのではないでしょうか。
+            <br></br>
+            <br></br>
+            私のコーチングでは、ヒアリングをもとに、<br></br>
+            できることから少しずつ習慣化できるようサポートします。
+          </Section.Body>
+        </Section>
+        <Section
+          img={"/images/life/meal.jpg"}
+          width={300}
+          height={300}
+        >
+          <Section.Title>詳細</Section.Title>
+          <Section.Body>
+            セッションは対面形式です。<br></br>
+            ＊オンライン希望の場合、ご相談ください。<br></br>
+            <br></br>
+            カフェなどお好きな場所をご指定ください。<br></br>
+            私はカフェ巡りが大好きなので、素敵なところで一緒にお話しできたらうれしいです。
+            <br></br>
+            ＊交通費や飲食代は料金に含まれませんので、ご了承ください。
+          </Section.Body>
+        </Section>
+        <Section
+          img={"/images/life/veg1.jpg"}
+          width={600}
+          height={600}
+        >
+          <Section.Title>1:1 90 Minute Session</Section.Title>
+          <Section.Body>
+          セッションの前日までにワークシートを記入していただきます。
+          <br></br>
+          <br></br>
+          記入していただいた内容をベースに、過去と現在の食生活、人間関係、環境の変化など多方面から今の自分を深掘りしていきます。{" "}
+          <br></br>
+          <br></br>
+          話を聞いてほしい、ちょっと試してみたいなど理由はなんでも構いません。
+          <br></br>
+          <br></br>1セッション50分<br></br>
+          初回無料、2回目以降3,000円。
+          </Section.Body>
+        </Section>
+      </div>
+    </Fragment>
+  );
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            layout: "health",
-        },
-    };
+  return {
+    props: {
+      layout: "health",
+    },
+  };
 }
