4696104f5212990fade3866911b62c62c42f50d9
milkandhoney995 <harada.g17@gmail.com>
2023-05-02 20:54:52 +0900

    CSS調整

--- pages/webdeveloper/portfolio/index.tsx
+++ pages/webdeveloper/portfolio/index.tsx
@@ -6,6 +6,8 @@ export default function Portofolio() {
   const modalList = [
     { cover: "/images/life/hana.jpg", alt: "debug", title: "タイトル", body: "吾輩は猫である。"},
     { cover: "/images/life/cafe.jpg", alt: "debug", title: "タイトル", body: "吾輩は猫である。"},
+    { cover: "/images/life/sunnyday.png", alt: "debug", title: "タイトル", body: "吾輩は猫である。"},
+    { cover: "/images/life/meal.jpg", alt: "debug", title: "タイトル", body: "吾輩は猫である。"},
   ];
 
   return (
