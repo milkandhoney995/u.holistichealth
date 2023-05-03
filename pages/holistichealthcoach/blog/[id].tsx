8f9a227b5a0d7554850805f7514a49e8d46fb3ae
milkandhoney995 <harada.g17@gmail.com>
2023-05-03 21:51:15 +0900

    modal, cardの修正

--- pages/holistichealthcoach/blog/[id].tsx
+++ pages/holistichealthcoach/blog/[id].tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Sidemenu from "../../../components/sidemenu/sidemenu"
-
-// import Tag from "../../../components/tag/tag";
 import { client } from "../../../libs/client";
 import { Fragment } from "react";
 
