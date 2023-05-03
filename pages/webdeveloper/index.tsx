7b5dc43e5511c6e703cbd3e10344bbdae8ed8b1a
milkandhoney995 <harada.g17@gmail.com>
2023-05-03 21:51:23 +0900

    文章追加

--- pages/webdeveloper/index.tsx
+++ pages/webdeveloper/index.tsx
@@ -12,6 +12,15 @@ export default function Home() {
         </Head>
         <div className="main">
           Hi! I'm <strong>Utano Harada</strong>, a Web Front-end Engineer.
+          Making the most of components in Vue/React.js and Sass styles, I would change the tedious scripts more easy to read, and give them higher maintainability.
+          <br />
+          <br />
+          I have worked on several open source apps, and been in charge of <strong>ensuring reusablility and componentizing React/Vue apps.</strong>
+          <br />
+          <br />
+          I'm passionate about simplifying and optimizing the intricate and lengthy script. I also have implemented new function(authentication, etc),  in previous project.
+          <br />
+          <strong>I'm currently looking for the Web/Mobiledevelopment.</strong>
         </div>
       </Fragment>
     </>
