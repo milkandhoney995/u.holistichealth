ccace4018ff64b7d844693e7b4722b5dea4480ef
milkandhoney995 <harada.g17@gmail.com>
2023-05-04 08:43:06 +0900

    GlassSection: menus props作成

--- components/layout/layout_web.tsx
+++ components/layout/layout_web.tsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
 import { GlassSection } from "../glassSection/glassSection";
 import classes from "./layout.module.scss";
+import { SvgHome, SvgLaptop, SvgWork } from "../icons/index";
 
 function LayoutWeb(props) {
+  const menus = [
+    { href: "/webdeveloper", title: "Home", icon: <SvgHome /> },
+    { href: "/webdeveloper/portfolio", title: "Portfolio", icon: <SvgWork /> },
+    { href: "/webdeveloper/profile", title: "Skill", icon: <SvgLaptop /> },
+  ];
+
   return (
     <Fragment>
       <main className={classes.main}>
         <div className={classes.main__glass}>
-          <GlassSection>
+          <GlassSection
+            menus={menus}
+          >
             <GlassSection.Title>
               ssss
             </GlassSection.Title>
